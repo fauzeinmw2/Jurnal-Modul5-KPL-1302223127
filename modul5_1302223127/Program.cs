74f6afbed41884d07a5aefc842235d7747457c42
Your Name <you@example.com>
2024-03-21 10:11:30 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul5_1302223127/Program.cs
+++ modul5_1302223127/Program.cs
@@ -1,7 +1,18 @@
 ﻿internal class Program
 {
+    public class Penjumlahan
+    {
+        public static void JumlahTigaAngka<T>(T a, T b, T c)
+        {
+            dynamic tempA = a;
+            dynamic tempB = b;
+            dynamic tempC = c;
+            T hasil = tempA + tempB + tempC;
+            Console.WriteLine($"Hasil Penjumlahan : {hasil}"); 
+        }
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Penjumlahan.JumlahTigaAngka<int>(13, 02, 22);
     }
 }
