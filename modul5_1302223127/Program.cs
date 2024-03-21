45fe7f3f39acb86595e540f3a5793a5ee3e6f1ee
Your Name <you@example.com>
2024-03-21 10:38:48 +0700

    menambahkan  class SimpleDataBase

--- modul5_1302223127/Program.cs
+++ modul5_1302223127/Program.cs
@@ -1,7 +1,36 @@
 ﻿internal class Program
 {
+
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+        public SimpleDataBase() {
+            this.storedData = new List<T>(); 
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            this.storedData.Add(data);
+            this.inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            Console.WriteLine("Isi Seluruh Data : ");
+            for (int i = 0; i < this.storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i + 1} berisi : {this.storedData[i]}, yang disimpan pada waktu UTC : {this.inputDates[i].ToString("dd/MM/yyyy hh:mm:ss tt")}");
+            }
+        }
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SimpleDataBase<int> objDB = new SimpleDataBase<int>();
+        objDB.AddNewData(13);
+        objDB.AddNewData(02);
+        objDB.AddNewData(22);
+        objDB.PrintAllData();
     }
 }
--- modul5_1302223127/obj/Debug/net8.0/modul5_1302223127.AssemblyInfo.cs
+++ modul5_1302223127/obj/Debug/net8.0/modul5_1302223127.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_1302223127")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2396bc602e2145bb41b7b1c11cb1c599906622")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_1302223127")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_1302223127")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
